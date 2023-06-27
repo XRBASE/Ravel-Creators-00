41eb0decd84bdf19348474cdf62347d8a42eda13
DESKTOP-OU76AT9\Glenn <glenn-hoff@hotmail.com>
2023-06-27 14:52:11 +0200

    [ADDS] NPC component

---
+++ Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/NPCComponent.cs
@@ -0,0 +1,118 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Base.Ravel.Creator.Components
+{
+    [AddComponentMenu("Ravel/NPC")]
+    public partial class NPCComponent : ComponentBase
+    {
+        public override ComponentData Data { get; }
+
+        [SerializeField, HideInInspector] private NPCData _data;
+
+        protected override void BuildComponents() {}
+
+        protected override void DisposeData(){}
+
+        /// <summary>
+        /// Set the anchor to the current position of the NPC and change the npc state to Idle
+        /// </summary>
+        public void IdleWalk()
+        {
+        }
+
+        public void FollowPlayer()
+        {
+        }
+
+        public void MoveToPlayer()
+        {
+        }
+
+        public void LookAtLocalPlayer()
+        {
+        }
+
+        public void Idle()
+        {
+        }
+
+        public void Emote(int index)
+        {
+        }
+
+        public void WalkToAnchor()
+        {
+        }
+
+        public void RunToAnchor()
+        {
+        }
+
+        public void TeleportToAnchor()
+        {
+        }
+
+        public void SetAnchor(Transform transform)
+        {
+        }
+
+        public void Jump()
+        {
+        }
+
+#if UNITY_EDITOR
+        [CustomEditor(typeof(NPCComponent))]
+        private class NPCComponentEditor : Editor
+        {
+            private NPCComponent _instance;
+            private SerializedProperty _data;
+            private SerializedProperty _animator;
+            private SerializedProperty _anchor;
+
+            public void OnEnable()
+            {
+                _instance = (NPCComponent) target;
+                _data = serializedObject.FindProperty("_data");
+                _animator = _data.FindPropertyRelative("animator");
+                _anchor = _data.FindPropertyRelative("anchor");
+            }
+
+            public override void OnInspectorGUI()
+            {
+                DrawDefaultInspector();
+
+                EditorGUI.BeginChangeCheck();
+
+                _instance._data.walkRadius = EditorGUILayout.FloatField("Walk Radius",
+                    _instance._data.walkRadius);
+                
+                _instance._data.idleWalkUpdateTimeOut = EditorGUILayout.FloatField("Idle Walk Update Timeout",
+                    _instance._data.idleWalkUpdateTimeOut);
+
+                EditorGUILayout.PropertyField(_animator);
+                
+                EditorGUILayout.PropertyField(_anchor);
+                
+                serializedObject.ApplyModifiedProperties();
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    EditorUtility.SetDirty(_instance);
+                }
+            }
+        }
+#endif
+    }
+
+
+    [Serializable]
+    public class NPCData : ComponentData
+    {
+        public float walkRadius = 3f;
+        public float idleWalkUpdateTimeOut = 5f;
+        public Animator animator;
+        public Transform anchor;
+    }
+}
