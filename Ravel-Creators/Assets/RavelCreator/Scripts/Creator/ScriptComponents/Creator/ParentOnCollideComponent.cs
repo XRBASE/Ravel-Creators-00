a3fdd8484bdf1f0c834ffead64c97f30e23e9837
DESKTOP-OU76AT9\Glenn <glenn-hoff@hotmail.com>
2023-07-05 17:51:15 +0200

    [EDITS] PR feedback

--- Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/ParentOnCollideComponent.cs
+++ Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/ParentOnCollideComponent.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace Base.Ravel.Creator.Components
@@ -5,10 +7,15 @@ namespace Base.Ravel.Creator.Components
     /// <summary>
     /// Adding this component to a GameObject with a collider makes the player move along with this object while colliding with it
     /// </summary>
-    [RequireComponent(typeof(Collider))]
+    [AddComponentMenu("Ravel/MovingPlatform")]
     public partial class ParentOnCollideComponent : ComponentBase
     {
-        public override ComponentData Data { get; }
+        public override ComponentData Data
+        {
+            get { return _data; }
+        }
+
+        [SerializeField, HideInInspector] private ParentOnCollideData _data;
 
         protected override void BuildComponents()
         {
@@ -18,5 +25,22 @@ namespace Base.Ravel.Creator.Components
         {
         }
 
+#if UNITY_EDITOR
+        [CustomEditor(typeof(ParentOnCollideComponent))]
+        private class ParentOnCollideComponentEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                GUILayout.Label(
+                    "Adding this component to a GameObject with a collider makes\nthe player move along with this object while colliding with it");
+                DrawDefaultInspector();
+            }
+#endif
+        }
+    }
+
+    [Serializable]
+    public class ParentOnCollideData : ComponentData
+    {
     }
 }
