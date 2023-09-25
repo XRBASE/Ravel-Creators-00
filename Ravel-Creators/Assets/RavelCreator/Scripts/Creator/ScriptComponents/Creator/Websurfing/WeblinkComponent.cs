87722478065f53bac3acc5d25bb8991b7e4a7289
Lotte <Lotte@ravel.world>
2023-09-25 18:30:49 +0200

    [ADDS] weblink component. [REMOVES] unused folder.

---
+++ Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/Websurfing/WeblinkComponent.cs
@@ -0,0 +1,56 @@
+using Base.Ravel.Creator.Components;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Component used to open a given webURL as the result of a trigger event.
+/// </summary>
+[AddComponentMenu("Ravel/Websurfing/Weblink component")]
+public class WeblinkComponent : ComponentBase
+{
+	public override ComponentData Data {
+		get { return null; }
+	}
+
+	protected override void BuildComponents() { } 
+	protected override void DisposeData() { }
+
+	[SerializeField] private string url;
+
+	public void OpenURL() { }
+
+	private void OpenUrlDirect() {
+		Application.OpenURL(url);
+	}
+
+#if UNITY_EDITOR
+	[CustomEditor(typeof(WeblinkComponent))]
+	private class WeblinkComponentEditor : Editor
+	{
+
+		public override void OnInspectorGUI() {
+			WeblinkComponent instance = (WeblinkComponent)target;
+
+			DrawDefaultInspector();
+			EditorGUILayout.BeginHorizontal();
+			if (!string.IsNullOrEmpty(instance.url) && 
+			    !(instance.url.StartsWith("https://") || instance.url.StartsWith("http://"))) {
+				
+				EditorGUILayout.HelpBox("Please add Https:// in front of your url, as the weblink will otherwise " +
+				                        "be opened as subdomain of Ravel (i.e. Https://Ravel.com/[URL]).", MessageType.Warning);
+				
+				if (GUILayout.Button("fix", GUILayout.Width(100), GUILayout.Height(EditorGUIUtility.singleLineHeight * 2f))) {
+					instance.url = "https://" + instance.url;
+				}
+			}
+			EditorGUILayout.EndHorizontal();
+			
+			if (GUILayout.Button("Open url")) {
+				instance.OpenUrlDirect();
+			}
+		}
+	}
+#endif
+}
