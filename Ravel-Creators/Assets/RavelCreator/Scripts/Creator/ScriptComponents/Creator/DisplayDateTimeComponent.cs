5e763dc3096dd3bc458f3c72bac95de2b8946b76
DESKTOP-OU76AT9\Glenn <glenn-hoff@hotmail.com>
2023-12-07 13:26:11 +0100

    [EDITS] feedback

--- Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/DisplayDateTimeComponent.cs
+++ Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/DisplayDateTimeComponent.cs
@@ -1,5 +1,4 @@
 using System;
-using Base.Ravel.Creator.Components;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
@@ -9,7 +8,7 @@ namespace Base.Ravel.Creator.Components
     /// <summary>
     /// Displays current date on textmeshprougui
     /// </summary>
-    [AddComponentMenu("Ravel/Display Date")]
+    [AddComponentMenu("Ravel/Display Date Time")]
     [HelpURL("https://www.notion.so/thenewbase/Display-Date-Component-e8fbf232ef944cc6b648390a27679e30?pvs=4")]
     public partial class DisplayDateTimeComponent : ComponentBase
     {
@@ -18,20 +17,33 @@ namespace Base.Ravel.Creator.Components
             get { return _data; }
         }
 
-        protected override void BuildComponents()
+        protected override void BuildComponents(){}
+
+        protected override void DisposeData(){}
+
+        public void ShowDateTime(){}
+        
+        public static string ParseDateTime(DisplayDateTimeData.DateTimeFormat format, string customFormat = default)
         {
+            switch (format)
+            {
+                case DisplayDateTimeData.DateTimeFormat.Date:
+                    return DateTime.Now.ToShortDateString();
+                case DisplayDateTimeData.DateTimeFormat.Time:
+                    return $"{DateTime.Now:HH:mm:ss}";
+                case DisplayDateTimeData.DateTimeFormat.LongDate:
+                    return DateTime.Now.ToLongDateString();
+                case DisplayDateTimeData.DateTimeFormat.DateTime:
+                    return $"{DateTime.Now.ToShortDateString()}, {DateTime.Now:HH:mm:ss}";
+                case DisplayDateTimeData.DateTimeFormat.Custom:
+                    return DateTime.Now.ToString(customFormat);
+                default:
+                    return String.Empty;
+            }
         }
 
-        protected override void DisposeData()
-        {
-        }
-
-        public void DisplayDate()
-        {
-        }
 
         [SerializeField, HideInInspector] private DisplayDateTimeData _data;
-
 
 #if UNITY_EDITOR
         [CustomEditor(typeof(DisplayDateTimeComponent))]
@@ -57,15 +69,12 @@ namespace Base.Ravel.Creator.Components
                 {
                     EditorGUILayout.PropertyField(_data.FindPropertyRelative("customDateTimeFormat"));
                 }
-
                 EditorGUILayout.PropertyField(_data.FindPropertyRelative("postfixText"));
                 if (GUILayout.Button("Preview Text"))
                 {
                     _instance._data.textMeshProUGUI.text =
-                        _instance._data.prefixText + ParseDateTime(_instance._data.dateTimeFormat) +
-                        _instance._data.postfixText;
+                        _instance._data.prefixText + ParseDateTime(_instance._data.dateTimeFormat, _instance._data.customDateTimeFormat) + _instance._data.postfixText;
                 }
-
                 serializedObject.ApplyModifiedProperties();
                 if (EditorGUI.EndChangeCheck())
                 {
@@ -73,30 +82,7 @@ namespace Base.Ravel.Creator.Components
                 }
             }
 
-            private string ParseDateTime(DisplayDateTimeData.DateTimeFormat format)
-            {
-                switch (format)
-                {
-                    case DisplayDateTimeData.DateTimeFormat.Date:
-                        return DateTime.Now.ToShortDateString();
-                        break;
-                    case DisplayDateTimeData.DateTimeFormat.Time:
-                        return $"{DateTime.Now:hh:mm:ss}";
-                        break;
-                    case DisplayDateTimeData.DateTimeFormat.LongDate:
-                        return DateTime.Now.ToLongDateString();
-                        break;
-                    case DisplayDateTimeData.DateTimeFormat.DateTime:
-                        return $"{DateTime.Now.ToShortDateString()}, {DateTime.Now:hh:mm:ss}";
-                        break;
-                    case DisplayDateTimeData.DateTimeFormat.Custom:
-                        return DateTime.Now.ToString(_instance._data.customDateTimeFormat);
-                        break;
-                    default:
-                        return String.Empty;
-                        break;
-                }
-            }
+
         }
 #endif
     }
