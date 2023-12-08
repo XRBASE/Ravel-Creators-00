d1eaa7bc7c7b4c17520fb28755ce936f98325b3e
Lotte <Lotte@ravel.world>
2023-12-08 13:28:18 +0100

    [ADDS] new floorquiz. [EDITS] moved floorquiz out of the TMP folder.

--- Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/FloorQuiz_Test/Answer/FloorAnswer.cs
+++ Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/FloorQuiz_Test/Answer/FloorAnswer.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -20,56 +19,19 @@ namespace QuizSampleFloor
         [SerializeField] private Color _highlight = new(1, 1, 0);
         [SerializeField] private Color _correct = new(0, 1, 0);
         [SerializeField] private Color _false = new(1, 0, 0);
-        private Color _original;
 
         [SerializeField] private TMP_Text _answerField;
         [SerializeField] private Material _sourceMaterial;
         [SerializeField] private MeshRenderer[] _highlightRenderers;
 
-        private Material _itemMaterial;
-        private bool _resultShown = false;
+        private void Awake() { }
 
-        private void Start() {
-            _itemMaterial = new Material(_sourceMaterial);
-            _itemMaterial.color = _sourceMaterial.color;
-            _original = _sourceMaterial.color;
+        public void Reset() { }
 
-            
-            for (int i = 0; i < _highlightRenderers.Length; i++) {
-                _highlightRenderers[i].material = _itemMaterial;
-            }
-        }
+        public void ShowResult(bool isCorrectAnswer) { }
 
-        public void Reset() {
-            _itemMaterial.color = _original;
-            HasVote = false;
-            onVoteChanged?.Invoke(false);
-            _resultShown = false;
-        }
+        private void OnTriggerEnter(Collider other) { }
 
-        public void ShowResult(bool isCorrectAnswer) {
-            //for now we change the material color here, but to make it more flexible we can add a visualizer here that 
-            //shows an animation, or changes the material or whatever
-            _itemMaterial.color = (isCorrectAnswer ? _correct : _false);
-            _resultShown = true;
-        }
-
-        private void OnTriggerEnter(Collider other) {
-            if (!_resultShown) {
-                _itemMaterial.color = _highlight;
-            }
-            
-            HasVote = true;
-            onVoteChanged?.Invoke(true);
-        }
-
-        private void OnTriggerExit(Collider other) {
-            if (!_resultShown) {
-                _itemMaterial.color = _original;
-            }
-
-            HasVote = false;
-            onVoteChanged?.Invoke(false);
-        }
+        private void OnTriggerExit(Collider other) { }
     }
 }
--- Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/FloorQuiz_Test/FloorQuiz.cs
+++ Ravel-Creators/Assets/RavelCreator/Scripts/Creator/ScriptComponents/Creator/FloorQuiz_Test/FloorQuiz.cs
@@ -3,25 +3,13 @@ using System.Collections.Generic;
 using Base.Ravel.Creator.Components;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace QuizSampleFloor
 {
     public class FloorQuiz : MonoBehaviour, IUniqueId
     {
-        public bool SetUniqueID {
-            get { return true; }
-        }
-
-        public int ID {
-            get { return _id; }
-            set { _id = value; }
-        }
-
-        private Question CurQuestion {
-            get { return _questions[_questionId]; }
-        }
-
         [SerializeField] private FloorAnswer _answerTemplate;
         [SerializeField] private TMP_Text _questionField;
         [SerializeField] private Button _submitBtn;
@@ -29,114 +17,40 @@ namespace QuizSampleFloor
         [SerializeField] private List<Question> _questions;
         [SerializeField] private bool _networked = false;
         
+        [SerializeField] private UnityEvent onQuizFinished;
+        [SerializeField] private UnityEvent onQuizReset;
+        
         [HideInInspector, SerializeField] private int _id;
         
-        private int _questionId = 0;
-        private State _state = State.ShowQuestion;
-
-        private List<FloorAnswer> _answerObjects;
-
-        private void Start() {
-            _answerObjects = new List<FloorAnswer>();
-
-            _submitBtn.onClick.AddListener(OnSubmit);
-
-            SetQuestion();
-
-            //set button interactable state
-            OnAnwerVoteChanged(false);
+        public bool SetUniqueID {
+            get { return true; }
         }
-
-        private void SetQuestion() {
-            for (int i = 0; i < _answerObjects.Count; i++) {
-                _answerObjects[i].gameObject.SetActive(false);
-            }
-
-            _questionField.text = CurQuestion.question;
-
-            if (!_answerObjects.Contains(_answerTemplate)) {
-                _answerObjects.Add(_answerTemplate);
-                _answerTemplate.onVoteChanged = OnAnwerVoteChanged;
-            }
-
-            for (int i = 0; i < Mathf.Max(CurQuestion.answers.Count, _answerObjects.Count); i++) {
-                if (i < CurQuestion.answers.Count) {
-                    if (i >= _answerObjects.Count) {
-                        FloorAnswer fa = Instantiate(_answerTemplate, _answerTemplate.transform.parent);
-                        fa.onVoteChanged = OnAnwerVoteChanged;
-
-                        _answerObjects.Add(fa);
-                    }
-
-                    _answerObjects[i].Answer = CurQuestion.answers[i];
-                    _answerObjects[i].gameObject.SetActive(true);
-                }
-                else {
-                    _answerObjects[i].Reset();
-                }
-            }
+        public int ID {
+            get { return _id;}
+            set { _id = value; }
         }
+        
+        public void OnReset() { }
 
-        private void OnAnwerVoteChanged(bool hasVote) {
-            if (hasVote || _state == State.ShowAnswer) {
-                _submitBtn.interactable = true;
-            }
-            else {
-                for (int i = 0; i < _answerObjects.Count; i++) {
-                    if (_answerObjects[i].gameObject.activeSelf && _answerObjects[i].HasVote) {
-                        _submitBtn.interactable = true;
-                        return;
-                    }
-                }
+        private void SetQuestion() { }
 
-                _submitBtn.interactable = false;
-            }
-        }
+        private void SetQuizFinished() { }
 
-        public void OnSubmit() {
-            if (_state == State.ShowQuestion) {
-                for (int i = 0; i < _answerObjects.Count; i++) {
-                    _answerObjects[i].ShowResult(i == CurQuestion.correctAnswer);
-                }
+        private void OnAnswerVoteChanged(bool hasVote) { }
 
-                _state = State.ShowAnswer;
-            }
-            else {
-                for (int i = 0; i < _answerObjects.Count; i++) {
-                    _answerObjects[i].Reset();
-                }
+        public void OnSubmit() { }
 
-                _questionId = (_questions.Count + (_questionId + 1)) % _questions.Count;
-                SetQuestion();
+        private void ShowAnswers() { }
 
-                _state = State.ShowQuestion;
-
-                //update button interactable
-                OnAnwerVoteChanged(false);
-            }
-        }
-
-        public void OnReset() {
-            for (int i = 0; i < _answerObjects.Count; i++) {
-                _answerObjects[i].Reset();
-            }
-
-            _state = State.ShowQuestion;
-        }
-
-        private enum State
-        {
-            ShowQuestion = 0,
-            ShowAnswer = 1
-        }
+        private void ShowQuestion() { }
     }
-
+    
     [Serializable]
     class Question
     {
         public string question;
         public List<string> answers;
-
+        
         public int correctAnswer;
     }
 }
